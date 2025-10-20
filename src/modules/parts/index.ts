701ecc68f2e4c0ff5831b53a8f8e1527c72ab4de
SW33TSTUFF <simaakniyaz@gmail.com>
2025-10-20 16:47:19 +0530

    feat: Remove parts module including service, controller, routes, types, and validation

--- src/modules/parts/index.ts
+++
@@ -1,4 +0,0 @@
-export { WorkOrderPartService } from './parts.service';
-export { WorkOrderPartController } from './parts.controller';
-export * from './parts.types';
-export { default as partsRouter } from './parts.routes';
